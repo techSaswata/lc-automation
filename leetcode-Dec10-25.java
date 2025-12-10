1c67b3cdf27fe789ecd8510ba933a2e166872128
techSaswata <saswata.24bcs10248@sst.scaler.com>
2025-12-10 02:24:46 +0000

    Add accepted LeetCode solution

--- leetcode-Dec10-25.java
+++ leetcode-Dec10-25.java
@@ -1,17 +1,17 @@
 class Solution {
     public int countPermutations(int[] complexity) {
-        long res = 1;
-        long mod = 1000000007L;
         int n = complexity.length;
-        int base = complexity[0];
-        
+        int rootComplexity = complexity[0];
         for (int i = 1; i < n; i++) {
-            if (complexity[i] <= base) {
+            if (complexity[i] <= rootComplexity) {
                 return 0;
             }
-            res = (res * i) % mod;
         }
-        
-        return (int) res;
+        long ans = 1;
+        long mod = 1000000007L;
+        for (int i = 2; i < n; i++) {
+            ans = (ans * i) % mod;
+        }
+        return (int) ans;
     }
 }
